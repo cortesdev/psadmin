9d5347e6d5d9068fb7a6442350a42d4e980a2470
Ricardo Cortes dos Santos <ricardo.cortesdossantos@diconium.com>
2018-12-04 15:06:23 +0100

    Innobln-460 add navigation and logo

---
+++ src/components/common/header.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var React = require('react');
+
+var Header = React.createClass({
+	render: function () {
+		return (
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid collapse navbar-collapse">
+                    <a href="/" className="navbar-brand">
+                        <img src="images/pluralsight-logo.png" alt="image"/>
+                    </a>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#about">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+		); 
+	}
+});
+
+module.exports = Header;
